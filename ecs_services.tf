5e663acdf058fd6cea87106610791eea560846ee
Eric Dahl <eric.c.dahl@gmail.com>
2017-12-03 14:34:04 -0800

    health check notes

--- ecs_services.tf
+++ ecs_services.tf
@@ -64,10 +64,28 @@ resource "aws_alb_target_group" "default" {
   vpc_id               = "${module.vpc.vpc_id}"
   port                 = 8080
   protocol             = "HTTP"
-  deregistration_delay = 10
+  deregistration_delay = 1
 
   health_check {
+    # The service task needs to start up and respond to health probes in
+    #
+    # unhealthy_threshold * (interval + [0, timeout])
+    #
+    # If the probes can't connect to the target then it's immediately recorded
+    # as a failed health check. This would happen if the target is initializing and
+    # hasn't started listening on the socket.
+    #
+    # e.g.,
+    #   timeout = 2, unhealthy_threshold = 3, interval = 5
+    # The target has between 15 and 21 seconds to respond to health checks depending
+    # on whether it's accepting connections.
+    #
+    # This does not seem to be exact. When using a task that takes a constant 30 seconds
+    # to accept connections, even some tasks managed to get through the health check after
+    # 20+ repeated failed attempts with 2/3/5.
+
     healthy_threshold = 2
+    unhealthy_threshold = 8
     interval = 5
     timeout = 2
   }
