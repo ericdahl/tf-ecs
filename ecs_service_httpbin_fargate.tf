da8b428daf05da4c63c09c1ab4fc2edd1b3a1890
Eric Dahl <eric.c.dahl@gmail.com>
2017-12-31 17:38:53 -0800

    Add fargate task/service

---
+++ ecs_service_httpbin_fargate.tf
@@ -0,0 +1,82 @@
+data "template_file" "httpbin_fargate" {
+  template = "${file("templates/tasks/httpbin-fargate.json")}"
+}
+
+resource "aws_ecs_task_definition" "httpbin_fargate" {
+  container_definitions = "${data.template_file.httpbin_fargate.rendered}"
+  family                = "httpbin-fargate"
+
+  requires_compatibilities = [
+    "FARGATE",
+  ]
+
+  network_mode = "awsvpc"
+  cpu          = 256
+  memory       = 512
+}
+
+resource "aws_ecs_service" "httpbin_fargate" {
+  name            = "httpbin-fargate"
+  cluster         = "${module.ecs.cluster_name}"
+  task_definition = "${aws_ecs_task_definition.httpbin_fargate.arn}"
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups = [
+      "${module.vpc.sg_allow_8080}",
+      "${module.vpc.sg_allow_egress}",
+      "${module.vpc.sg_allow_vpc}"
+    ]
+
+    subnets = [
+      "${module.vpc.subnet_private1}"
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = "${aws_alb_target_group.httpbin_fargate.arn}"
+    container_name = "httpbin"
+    container_port = 8080
+  }
+}
+
+
+resource "aws_alb" "httpbin_fargate" {
+  subnets = [
+    "${module.vpc.subnet_public1}",
+    "${module.vpc.subnet_public2}",
+    "${module.vpc.subnet_public3}",
+  ]
+
+  security_groups = [
+    "${module.vpc.sg_allow_egress}",
+    "${module.vpc.sg_allow_80}"
+  ]
+}
+
+resource "aws_alb_listener" "httpbin_fargate" {
+  "default_action" {
+    target_group_arn = "${aws_alb_target_group.httpbin_fargate.arn}"
+    type             = "forward"
+  }
+
+  load_balancer_arn = "${aws_alb.httpbin_fargate.arn}"
+  port              = 80
+}
+
+resource "aws_alb_target_group" "httpbin_fargate" {
+  vpc_id               = "${module.vpc.vpc_id}"
+  port                 = 8080
+  protocol             = "HTTP"
+  deregistration_delay = 5
+  target_type          = "ip"
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 10
+    interval = 5
+    timeout = 2
+  }
+}
+
