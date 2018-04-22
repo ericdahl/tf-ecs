a49f949e257f0c1c14cdab7c521da8f89a92a2cc
Eric Dahl <eric.c.dahl@gmail.com>
2018-04-21 20:54:04 -0700

    add notes about Task roles

--- ecs_service_demo_iam_ec2.tf
+++ ecs_service_demo_iam_ec2.tf
@@ -13,6 +13,32 @@ resource "aws_ecs_task_definition" "demo_iam" {
 
   container_definitions = "${data.template_file.demo_iam.rendered}"
   family                = "demo_iam"
+
+
+  # IAM Task role granting access to S3 list operation
+  # Inside the container, AWS SDK will
+  #
+  # curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
+  #
+  # where `AWS_CONTAINER_CREDENTIALS_RELATIVE_URI` is an env variable populated
+  # by ECS which looks like
+  #
+  # /v2/credentials/8fb1bd0d-9bdd-4de5-9735-59349f935bd7
+  #
+  # This returns a JSON response with a key/secret/temp token, similar to the
+  # standard EC2 metadata IAM endpoint.
+  #
+  # Typical ECS container host then has iptables rules to pre-route/redirect
+  #
+  # 169.254.170.2/32 --> 127.0.0.1:51679
+  #
+  # ecs-agent docker container listens at 127.0.0.1:51679 (host networking)
+  #
+  # Every AWS SDK is expected to check for `$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI`
+  # and query it for credentials (example: https://github.com/aws/aws-sdk-ruby/blob/fd0373ea9/gems/aws-sdk-core/lib/aws-sdk-core/credential_provider_chain.rb#L86)
+
+  # more info at https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-iam-roles.html
+
   task_role_arn         = "${aws_iam_role.demo_iam.arn}"
 }
 
