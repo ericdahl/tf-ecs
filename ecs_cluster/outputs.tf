44e1c1fde503e5a1f03b5f55fad90a98b9c7cf57
Eric Dahl <eric.c.dahl@gmail.com>
2020-05-10 11:11:05 -0700

    TF 0.12 upgrade

--- ecs_cluster/outputs.tf
+++ ecs_cluster/outputs.tf
@@ -1,23 +1,23 @@
 output "ami_id" {
-  value = "${data.aws_ami.ecs.id}"
+  value = data.aws_ami.ecs.id
 }
 
 output "iam_role_ecs_service_name" {
-  value = "${aws_iam_role.ecs_service.name}"
+  value = aws_iam_role.ecs_service.name
 }
 
 output "iam_role_fleet_arn" {
-  value = "${aws_iam_role.fleet.arn}"
+  value = aws_iam_role.fleet.arn
 }
 
 output "iam_instance_profile_name" {
-  value = "${aws_iam_instance_profile.default.name}"
+  value = aws_iam_instance_profile.default.name
 }
 
 output "user-data" {
-  value = "${data.template_file.cloud_init.rendered}"
+  value = data.template_file.cloud_init.rendered
 }
 
 output "cluster_name" {
-  value = "${aws_ecs_cluster.default.name}"
+  value = aws_ecs_cluster.default.name
 }
